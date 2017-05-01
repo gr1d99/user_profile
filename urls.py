a6d660c518dcdbebd77e5d1c2b483528d21d414d
gr1d99 <gideonkimutai9@gmail.com>
2017-05-01 17:42:57 +0300

    changed url naming

--- urls.py
+++ urls.py
@@ -6,7 +6,6 @@ app_name = 'user_profile'
 
 
 urlpatterns = [
-    url(r'^$', views.Index.as_view(), name='userprofile_index'),
-    url(r'^profile/(?P<slug>[A-Za-z0-9_-]+)$', views.UserProfileDetailView.as_view(), name='userprofile_detail'),
-    url(r'^profile/(?P<slug>[A-Za-z0-9_-]+)/update/$', views.UpdateUserProfileView.as_view(), name='userprofile_update'),
+    url(r'^(?P<slug>[A-Za-z0-9_-]+)$', views.UserProfileDetailView.as_view(), name='userprofile_detail'),
+    url(r'^(?P<slug>[A-Za-z0-9_-]+)/update/$', views.UpdateUserProfileView.as_view(), name='userprofile_update'),
 ]
